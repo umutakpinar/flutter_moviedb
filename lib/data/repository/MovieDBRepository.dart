06a3e5953961a3ecc04ea770ec595e81115c6e62
Umut <57362066+umutakpinar@users.noreply.github.com>
2024-02-29 02:38:56 +0300

    To Do note added

--- lib/data/repository/MovieDBRepository.dart
+++ lib/data/repository/MovieDBRepository.dart
@@ -1,5 +1,8 @@
 import 'package:flutter_moviedb/data/service/api/moviedb.dart';
 
+//  TO DO : BU ALANDA DATAYI LOCAL OLARAK KAYDEDİP, VERİLERİ DE BURADAN ÇEKECEĞİM.
+// Not : SharedPreferences büyük dosyaları kaydetmiyor kendi model classıma izinv ermyebilir bu durumda SQLite.
+
 class MovieDBRepository {
   final MovieDBAPI _api = MovieDBAPI();
   
